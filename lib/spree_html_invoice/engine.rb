7b7dcc43e0b2a4190d7931925883b439d2656712
Michael Bianco <iloveitaly@gmail.com>
2013-02-18 16:51:01 -0500

    Fixing decorator loading & engine cleanup

--- lib/spree_html_invoice/engine.rb
+++ lib/spree_html_invoice/engine.rb
@@ -2,6 +2,8 @@ module Spree::HtmlInvoice; end
 
 module SpreeHtmlInvoice
   class Engine < Rails::Engine
+    require 'spree/core'
+    isolate_namespace Spree
     engine_name 'spree_html_invoice'
 
     config.autoload_paths += %W(#{config.root}/lib)
@@ -15,7 +17,7 @@ module SpreeHtmlInvoice
     end
 
     def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), '../../../app/**/*_decorator*.rb')) do |c|
+      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
     end
