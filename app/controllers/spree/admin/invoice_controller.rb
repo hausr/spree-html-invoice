96129b6061462e1bad3f8ee09001d4b728933422
hausr <marcohaus@gmail.com>
2013-03-05 14:52:42 +0100

    Access denied for everyone except admins

--- app/controllers/spree/admin/invoice_controller.rb
+++ app/controllers/spree/admin/invoice_controller.rb
@@ -1,11 +1,17 @@
 module Spree
   module Admin
     class InvoiceController < Spree::BaseController
+      rescue_from CanCan::AccessDenied, :with => :render_404
+      
       def lasku
         template = params[:template]
         eval "@#{template} = true"
         @order = Spree::Order.find_by_number(params[:id])
-        render  template , :layout => false
+        if current_user && current_user.has_role?("admin")
+          render  template, :layout => false
+        else
+          raise CanCan::AccessDenied 
+        end 
       end
     end
   end
